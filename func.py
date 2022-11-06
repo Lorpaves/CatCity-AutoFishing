6a3869ebd5d85a0b2ff0cb346762b498e507424d
lorpaves <kirass1230@163.com>
2022-11-06 19:32:32 +0800

    fix bugs

---
+++ func.py
@@ -0,0 +1,205 @@
+import time
+from utility import util
+import json
+# try:
+#     with open('self.data.json', 'r', encoding='utf-8') as f:
+#         data = json.load(f)
+# except:
+#     raise FileExistsError('配置文件不存在')
+
+class fishing:
+    def __init__(self,ftimes,fpoint,data):
+        self.data = data
+        self.times = ftimes
+        self.point = fpoint
+        self.util = util(self.data)
+        self.fingshing_point_x,self.fingshing_point_y = self.data['DEFAULT']['钓鱼场']
+        self.back_x,self.back_y = self.data['DEFAULT']['返回']
+        self.start_btn_x,self.start_btn_y = self.data['FISHING']['开始钓鱼按钮']
+        self.duration = self.data['FISHING']['篮圈点击延迟']
+
+        self.diaoshou_x0, self.diaoshou_y0, self.diaoshou_x1, self.diaoshou_y1 = self.data['FISHING']['钓手信息']
+        self.a_x0,self.a_y0, self.a_x1, self.a_y1 =  self.data['FISHING']['钓竿']
+        self.b_x0,self.b_y0, self.b_x1, self.b_y1 =  self.data['FISHING']['线轮']
+        self.c_x0,self.c_y0, self.c_x1, self.c_y1 =  self.data['FISHING']['鱼线']
+        self.d_x0,self.d_y0, self.d_x1, self.d_y1 =  self.data['FISHING']['鱼饵']
+        self.e_x0,self.e_y0, self.e_x1, self.e_y1 =  self.data['FISHING']['鱼票']
+        self.diaogan_x,self.diaogan_y = self.data['FISHING']['购买位置']['钓竿位置']['pos']
+        self.xianlun_x,self.xianlun_y = self.data['FISHING']['购买位置']['线轮位置']['pos']
+        self.yuxian_x,self.yuxian_y = self.data['FISHING']['购买位置']['鱼线位置']['pos']
+        self.yu_er_x,self.yu_er_y = self.data['FISHING']['购买位置']['鱼饵位置']['pos']
+        self.fix_x, self.fix_y = self.data['FISHING']['购买位置']['维修']['pos']
+        self.add_x, self.add_y = self.data['FISHING']['购买位置']['鱼线和鱼饵购买位置']['数量增加按钮']
+        self.purchase_x, self.purchase_y= self.data['FISHING']['购买位置']['鱼线和鱼饵购买位置']['购买按钮']
+        self.confirm_x, self.confirm_y = self.data['FISHING']['购买位置']['鱼线和鱼饵购买位置']['确定购买按钮']
+
+    
+    def addCheck(self):
+        while 1:
+            if self.util.OCR(self.diaoshou_x0, self.diaoshou_y0, self.diaoshou_x1, self.diaoshou_y1) == '钓手信息':
+                a = self.util.OCR(self.a_x0,self.a_y0, self.a_x1, self.a_y1)
+                b = self.util.OCR(self.b_x0,self.b_y0, self.b_x1, self.b_y1)
+                c = self.util.OCR(self.c_x0,self.c_y0, self.c_x1, self.c_y1)
+                d = self.util.OCR(self.d_x0,self.d_y0, self.d_x1, self.d_y1)
+                e = self.util.OCR(self.e_x0,self.e_y0, self.e_x1, self.e_y1)
+                if e == '0':
+                    return 0
+                if (a, b, c, d).count('0') == True:
+                    self.util.mouseClick(self.diaogan_x,self.diaogan_y)
+                    time.sleep(1.5)
+                    self.util.mouseClick(self.fix_x, self.fix_y)
+                    time.sleep(1)
+                    self.util.mouseClick(self.xianlun_x,self.xianlun_y)
+                    time.sleep(1)
+                    self.util.mouseClick(self.fix_x, self.fix_y)
+                    time.sleep(1)
+                    if c == '0':
+                        self.util.doubleClick(self.yuxian_x,self.yuxian_y)
+                        time.sleep(1)
+                        self.util.doubleClick(self.fix_x, self.fix_y)
+                        for _ in range(4):
+                            self.util.doubleClick(self.add_x, self.add_y)
+                        time.sleep(1)
+                        self.util.doubleClick(self.purchase_x, self.purchase_y)
+                        time.sleep(1)
+                        self.util.doubleClick(self.confirm_x, self.confirm_y)
+                    if d == '0':
+                        self.util.doubleClick(self.yu_er_x,self.yu_er_y)  
+                        time.sleep(1)
+                        self.util.doubleClick(self.fix_x, self.fix_y)  
+                        for _ in range(4):
+                            self.util.doubleClick(self.add_x, self.add_y)
+                        time.sleep(1)
+                        self.util.doubleClick(self.purchase_x, self.purchase_y)
+                        time.sleep(1)
+                        self.util.doubleClick(self.confirm_x, self.confirm_y)
+                    self.util.doubleClick(self.back_x,self.back_y)
+                if (a,b,c,d).count('0') == False:
+                    break
+
+    def pull(self):
+        self.addCheck()
+        time.sleep(1)
+        self.util.doubleClick(self.start_btn_x,self.start_btn_y)
+        time.sleep(self.duration)
+        self.util.doubleClick(self.start_btn_x,self.start_btn_y)
+
+    def drag(self):
+        blue_area = self.data['FISHING']['钓鱼图片识别位置']['蓝色区域图片的路径']
+        blue_area_x0,blue_area_y0,blue_area_x1,blue_area_y1 = self.data['FISHING']['钓鱼图片识别位置']['蓝色区域']
+        green_area = self.data['FISHING']['钓鱼图片识别位置']['绿色区域图片的路径']
+        green_area_x0,green_area_y0,green_area_x1,green_area_y1 = self.data['FISHING']['钓鱼图片识别位置']['绿色区域']
+        red_area = self.data['FISHING']['钓鱼图片识别位置']['红色区域图片的路径']
+        red_area_x0,red_area_y0,red_area_x1,red_area_y1 = self.data['FISHING']['钓鱼图片识别位置']['红色区域']
+        
+        continue_x0,continue_y0,continue_x1,continue_y1 = self.data['FISHING']['点击屏幕继续']
+        escape_x0,escape_y0,escape_x1,escape_y1 = self.data['FISHING']['鱼儿跑了']
+        bottle_x0, bottle_y0,bottle_x1, bottle_y1 = self.data['FISHING']['来自异世界的瓶子']
+
+        bottle_area_x,bottle_area_y = self.data['FISHING']['漂流瓶位置']
+        bottle_confirm_x,bottle_confirm_y = self.data['FISHING']['漂流瓶确定']
+        
+        blank_x, blank_y = self.data['FISHING']['空白区域']
+        confirm_ctn_x,confirm_ctn_y = self.data['FISHING']['点击屏幕']
+        
+        while 1:
+            if self.util.findPic(blue_area_x0,blue_area_y0,blue_area_x1,blue_area_y1,blue_area,0.98):
+                self.util.mouseDown(self.start_btn_x,self.start_btn_y)
+            elif self.util.findPic(green_area_x0,green_area_y0,green_area_x1,green_area_y1,green_area,0.99):
+                self.util.mouseDown(self.start_btn_x,self.start_btn_y)
+            elif self.util.findPic(red_area_x0,red_area_y0,red_area_x1,red_area_y1,red_area,0.97):
+                self.util.mouseUp(self.start_btn_x,self.start_btn_y)   
+            if self.util.OCR(continue_x0,continue_y0,continue_x1,continue_y1) == '点击屏幕继续':
+                time.sleep(0.5)
+                self.util.doubleClick(confirm_ctn_x,confirm_ctn_y)
+                time.sleep(3)
+                self.util.doubleClick(confirm_ctn_x,confirm_ctn_y)
+                time.sleep(3)
+                self.util.doubleClick(blank_x, blank_y)  
+                if self.util.OCR(bottle_x0, bottle_y0,bottle_x1, bottle_y1) == '来自异世界的瓶子':
+                    time.sleep(2)
+                    self.util.doubleClick(bottle_area_x,bottle_area_y)
+                    time.sleep(4)
+                    self.util.doubleClick(bottle_confirm_x,bottle_confirm_y)
+                break
+            if self.util.OCR(escape_x0,escape_y0,escape_x1,escape_y1) == '鱼儿跑了':
+                time.sleep(2)
+                break
+
+    def fjoin(self, point):
+        time.sleep(1.5)
+        self.util.doubleClick(self.fingshing_point_x,self.fingshing_point_y)
+        time.sleep(2)
+        points = self.data['FISHING']['钓鱼点位置']
+        if point>3:
+            time.sleep(2)
+            self.util.swipeDown()
+            time.sleep(4)
+        self.util.doubleClick(points[point][0],points[point][1])
+
+    def fishing(self):
+        _times = 0
+        self.fjoin(self.point)
+        time.sleep(2)
+        while _times < self.times:
+            self.pull()
+            time.sleep(2)
+            self.drag()
+            _times += 1
+class tuya:
+    def __init__(self,tchoice,tdif,ttimes,data):
+        self.util = util(data)
+        self.data = data
+        self.action_x,self.action_y = self.data['DEFAULT']['行动']
+        self.tuya_x,self.tuya_y = self.data['DEFAULT']['涂鸦']
+
+        self.choice = tchoice
+        self.dif = tdif
+        self.times = ttimes
+        self.choiceLoc = self.data['TUYA']['副本位置']
+        self.difLoc = self.data['TUYA']['涂鸦难度']
+
+        self.invt_x,self.invt_y = self.data['TUYA']['开始调查']
+        self.start_x,self.start_y = self.data['TUYA']['进入挑战后的确认']
+        self.bt_result_x0,self.bt_result_y0,self.bt_result_x1,self.bt_result_y1=self.data['TUYA']['战斗结果区域']
+        self.again_x,self.again_y = self.data['TUYA']['再次挑战']
+        self.quit_x,self.quit_y = self.data['TUYA']['退出挑战']
+    def bt(self):
+        _times = 0
+
+        while _times < self.times:
+            text = self.util.OCR(self.bt_result_x0,self.bt_result_y0,self.bt_result_x1,self.bt_result_y1)
+            if text == '战斗胜利':
+                _times += 1
+                time.sleep(2)
+                self.util.doubleClick(self.again_x,self.again_y)
+            elif text == '战斗失败':
+                self.bt_restart()
+
+    def bt_restart(self):
+        time.sleep(2)
+        self.util.doubleClick(self.quit_x,self.quit_y)  #退出
+        time.sleep(5)
+        self.util.doubleClick(self.start_x,self.start_y)  #开始挑战
+        time.sleep(2)
+        self.util.doubleClick(self.start_x,self.start_y)  #确认
+
+    def tjoin(self):
+        time.sleep(2)
+        self.util.doubleClick(self.action_x,self.action_y)  #点行动
+        time.sleep(2)
+        self.util.doubleClick(self.tuya_x,self.tuya_y)   #涂鸦
+        time.sleep(2)
+        cloc_x, cloc_y = self.choiceLoc[self.choice]
+        dloc_x, dloc_y = self.difLoc[self.dif]
+        self.util.doubleClick(cloc_x, cloc_y )
+        time.sleep(2)
+        self.util.doubleClick(dloc_x, dloc_y )
+        time.sleep(2)
+        self.util.doubleClick(self.invt_x,self.invt_y)
+        time.sleep(2)
+        self.util.doubleClick(self.start_x,self.start_y)
+
+    def stuya(self):
+        self.tjoin()
+        self.bt()
---
+++ utility.py
@@ -0,0 +1,142 @@
+from io import BytesIO
+import time
+from PIL import Image
+import win32api ,win32con, win32gui, win32ui
+import ddddocr
+import cv2 as cv
+import numpy as np
+
+class util:
+    def __init__(self, data):
+        self.data = data
+        name = data['DEFAULT']['模拟器名称']
+        self.ocr = ddddocr.DdddOcr()
+        hwnd = win32gui.FindWindow(None,name)
+        self.hwnd = win32gui.FindWindowEx(hwnd,None,None,None)
+        x_1, y_1, x_2, y_2 = win32gui.GetWindowRect(hwnd)
+        self.width = x_2 - x_1
+        self.height = y_2 - y_1
+
+    def cvRead(self,img_path):
+        return cv.imdecode(np.fromfile(img_path, dtype=np.uint8), -1)
+
+    def mouseDown(self,x:int, y:int):
+        press_pos = win32api.MAKELONG(x,y)
+        win32gui.PostMessage(self.hwnd, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, press_pos)
+
+    def mouseUp(self,x:int, y:int):
+        press_pos = win32api.MAKELONG(x,y)
+        win32gui.PostMessage(self.hwnd, win32con.WM_LBUTTONUP,win32con.MK_LBUTTON, press_pos)
+
+    def mouseClick(self,x:int, y:int):
+        press_pos = win32api.MAKELONG(x,y)
+        win32gui.PostMessage(self.hwnd, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, press_pos)
+        time.sleep(0.2)
+        win32gui.PostMessage(self.hwnd, win32con.WM_LBUTTONUP,None, press_pos)
+
+    def doubleClick(self,x:int, y:int):
+        self.mouseClick(x, y)
+        time.sleep(0.1)
+        self.mouseClick(x, y)
+
+    def swipeDown(self):
+        win32gui.SendMessage(self.hwnd, win32con.WM_KEYDOWN,win32con.VK_SPACE,0)
+        time.sleep(1.2)
+        win32gui.SendMessage(self.hwnd, win32con.WM_KEYUP,win32con.VK_SPACE,0)
+
+    def swipeLeft(self):
+        win32gui.SendMessage(self.hwnd, win32con.WM_KEYDOWN,win32con.VK_CONTROL,0)
+        time.sleep(1.1)
+        win32gui.SendMessage(self.hwnd, win32con.WM_KEYUP,win32con.VK_CONTROL,0)
+
+    def capWindow(self):
+        wDC = win32gui.GetWindowDC(self.hwnd)
+        dcObj = win32ui.CreateDCFromHandle(wDC)
+        cDC = dcObj.CreateCompatibleDC()
+        dataBitMap = win32ui.CreateBitmap()
+        dataBitMap.CreateCompatibleBitmap(dcObj, self.width, self.height)
+        cDC.SelectObject(dataBitMap)
+        cDC.BitBlt((0, 0), (self.width, self.height), dcObj, (0, 0), win32con.SRCCOPY)
+        bmpstr = dataBitMap.GetBitmapBits(True)
+        img = np.fromstring(bmpstr, dtype='uint8')
+        img.shape = (self.height,self.width, 4)
+        img = img[...,:3]
+        img = np.ascontiguousarray(img)
+        dcObj.DeleteDC()
+        cDC.DeleteDC()
+        win32gui.ReleaseDC(self.hwnd, wDC)
+        win32gui.DeleteObject(dataBitMap.GetHandle())
+        return img
+
+    def capWindowPos(self, x:int, y:int, x1:int, y1:int):
+        w = x1 - x
+        h = y1 - y
+        wDC = win32gui.GetWindowDC(self.hwnd)
+        dcObj = win32ui.CreateDCFromHandle(wDC)
+        cDC = dcObj.CreateCompatibleDC()
+        dataBitMap = win32ui.CreateBitmap()
+        dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
+        cDC.SelectObject(dataBitMap)
+        cDC.BitBlt((0, 0), (w, h), dcObj, (x,y), win32con.SRCCOPY)
+        bmpstr = dataBitMap.GetBitmapBits(True)
+        img = np.fromstring(bmpstr, dtype='uint8')
+        img.shape = (h, w, 4)
+        img = img[...,:3]
+        img = np.ascontiguousarray(img)
+        dcObj.DeleteDC()
+        cDC.DeleteDC()
+        win32gui.ReleaseDC(self.hwnd, wDC)
+        win32gui.DeleteObject(dataBitMap.GetHandle())
+        return img
+
+    def OCR(self,x,y,x1,y1):
+        w = x1 - x
+        h = y1 - y
+        wDC = win32gui.GetWindowDC(self.hwnd)
+        dcObj = win32ui.CreateDCFromHandle(wDC)
+        cDC = dcObj.CreateCompatibleDC()
+        dataBitMap = win32ui.CreateBitmap()
+        dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
+        cDC.SelectObject(dataBitMap)
+        cDC.BitBlt((0, 0), (w, h), dcObj, (x,y), win32con.SRCCOPY)
+        bmpinfo = dataBitMap.GetInfo()
+        bmpstr = dataBitMap.GetBitmapBits(True)
+        img = Image.frombuffer(
+            'RGB',
+            (bmpinfo['bmWidth'], bmpinfo['bmHeight']),
+            bmpstr, 'raw', 'BGRX', 0, 1)
+        img_byte = BytesIO()  
+        img.save(img_byte, format='PNG')
+        img_info = img_byte.getvalue()
+        result = self.ocr.classification(img_info)
+        dcObj.DeleteDC()
+        cDC.DeleteDC()
+        win32gui.ReleaseDC(self.hwnd, wDC)
+        win32gui.DeleteObject(dataBitMap.GetHandle())
+        return result
+
+    def clickPic(self,tmp:str, threshold:float):
+        scr = self.capWindow()
+        tp = self.cvRead(tmp)
+        result = cv.matchTemplate(scr, tp, cv.TM_CCORR_NORMED)
+        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+        if max_val < threshold:
+            print(tmp, max_val, max_loc)
+            return False
+        self.mouseDown(int(max_loc[0]) + int(tp.shape[1]/2), int(max_loc[1]) + int(tp.shape[0]/2))
+        time.sleep(0.5)
+        self.mouseUp(int(max_loc[0]) + int(tp.shape[1]/2), int(max_loc[1]) + int(tp.shape[0]/2))
+        return True
+        
+    def findPic(self, x:int, y:int, x1:int, y1:int, tmp:str, threshold:float):
+        scr = self.capWindowPos(x,y,x1,y1)
+        tp = self.cvRead(tmp)
+        result = cv.matchTemplate(scr, tp, cv.TM_CCORR_NORMED)
+        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+        if max_val < threshold:
+            print(tmp, max_val, max_loc)
+            return False
+        print(tmp, max_val, max_loc)
+        return True
+
+
